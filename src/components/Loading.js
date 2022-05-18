553091fab29a79c9b4792791ad9e74930ee8b245
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-19 04:53:44 +0600

    redirection added after successfull login

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Loading = () => {
     return (
-        <div style={{height: "60vh", display: 'flex', justifyContent: 'center', alignItems: 'center'}} className="spinner-border text-success" role="status">
-            <span className="visually-hidden">Loading...</span>
+        <div style={{height: "60vh", display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
+            <div className="spinner-border text-success" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
         </div>
     );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import auth from "../firebase.init";
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import Loading from "./Loading";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    useEffect(() => {
+        if (user) {
+            navigate(from, { replace: true });
+        }
+    }, [user]);
+
+    if (loading) {
+        return <Loading></Loading>;
+    }
     return (
-        <div className="d-flex justify-content-center mt-5">
-            <div>
-                <h2>Please Login</h2>
-                <button onClick={() => signInWithGoogle()} className="btn btn-primary">
-                    Continue with Google
-                </button>
+        <>
+            <div className="d-flex justify-content-center mt-5">
+                <div>
+                    <h2>Please Login</h2>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-primary"
+                    >
+                        Continue with Google
+                    </button>
+                </div>
             </div>
-        </div>
+
+            {error && (
+                <p className="text-center text-danger mt-3">{error.message}</p>
+            )}
+        </>
     );
 };
 
