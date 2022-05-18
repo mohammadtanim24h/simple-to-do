bd64fe62f8edbd5b9af68b653e148d7be9892fed
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-19 04:09:00 +0600

    google login added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import auth from "../firebase.init";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     return (
-        <div>
-            <h2>Please Login</h2>
+        <div className="d-flex justify-content-center mt-5">
+            <div>
+                <h2>Please Login</h2>
+                <button onClick={() => signInWithGoogle()} className="btn btn-primary">
+                    Continue with Google
+                </button>
+            </div>
         </div>
     );
 };
